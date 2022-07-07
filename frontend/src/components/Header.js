28ca8b3240f2ef0ca3695092da0ba421449f75f5
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2022-07-07 10:42:10 +0530

    finishing header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,21 +8,31 @@ import {
 	useMediaQuery,
 	useTheme,
 } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import AddBusinessRoundedIcon from '@mui/icons-material/AddBusinessRounded';
 import DrawerComp from './Drawer';
 const Header = () => {
+	var location = useLocation().pathname;
+	// console.log(location.includes('/auth/'));
+	// console.log(location.includes('/auth/'));
+	// const pages = ['Home', 'Queries', 'Programmes', 'Logout'];
+	const auth = location.includes('/auth/');
 	const pages = ['Home', 'Services', 'AboutUs', 'ContactUs'];
+	if (auth) {
+		pages[1] = 'queries';
+		pages[2] = 'programmes';
+		pages[3] = 'logout';
+	}
 	const [value, setValue] = useState(0);
 	const theme = useTheme();
-	console.log(theme);
+	// console.log(theme);
 	const isMatch = useMediaQuery(theme.breakpoints.down('md'));
-	console.log(isMatch);
 	return (
 		<React.Fragment>
-			<AppBar sx={{ background: '#063970' }}>
+			<AppBar sx={{ background: '#93C3CE' }}>
 				<Toolbar>
-					<AddBusinessRoundedIcon sx={{ transform: 'scale(2)' }} />
+					<img src='/logo.png' height='50px' alt='logo' />
+					{/* <AddBusinessRoundedIcon sx={{ transform: 'scale(2)' }} /> */}
 					{isMatch ? (
 						<>
 							<Typography sx={{ fontSize: '2rem', paddingLeft: '10%' }}>
@@ -35,19 +45,28 @@ const Header = () => {
 							<Tabs
 								sx={{ marginLeft: 'auto' }}
 								indicatorColor='secondary'
-								textColor='#inherit'
 								value={value}
 								onChange={(e, value) => setValue(value)}
 							>
-								{pages.map((pages, index) => (
-									<Link
-										style={{ color: 'inherit', textDecoration: 'none' }}
-										to={`/${pages}`}
-										key={index}
-									>
-										<Tab label={pages} key={index}></Tab>
-									</Link>
-								))}
+								{auth
+									? pages.map((pages, index) => (
+											<Link
+												style={{ color: 'inherit', textDecoration: 'none' }}
+												to={`/auth/${pages}`}
+												key={index}
+											>
+												<Tab label={pages} key={index}></Tab>
+											</Link>
+									  ))
+									: pages.map((pages, index) => (
+											<Link
+												style={{ color: 'inherit', textDecoration: 'none' }}
+												to={`/${pages}`}
+												key={index}
+											>
+												<Tab label={pages} key={index}></Tab>
+											</Link>
+									  ))}
 							</Tabs>
 						</>
 					)}
