121067dee86cd1fc6c3f4a86cf9b9b53252b0a72
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2022-07-07 10:41:04 +0530

    changes in routes for handling images

--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -1,18 +1,31 @@
-const express = require("express");
-const newsController = require("../controllers/newsController");
-const authController = require("../controllers/authController");
+const express = require('express');
+const newsController = require('../controllers/newsController');
+const authController = require('../controllers/authController');
 const router = express.Router();
 
-router.route("/").get(newsController.getAllNews).post(
-  newsController.createNews,
-);
+router
+	.route('/')
+	.get(newsController.getAllNews)
+	.post(
+		authController.protect,
+		authController.restrictTo('admin'),
+		newsController.createNews,
+	);
 
-router.route("/:id").get(newsController.getNews).patch(
-  newsController.updateNews,
-).delete(
-  authController.protect,
-  authController.restrictTo("admin"),
-  newsController.deleteNews,
-);
+router
+	.route('/:id')
+	.get(newsController.getNews)
+	.patch(
+		authController.protect,
+		authController.restrictTo('admin'),
+		newsController.uploadNewsImages,
+		newsController.resizeNewsImages,
+		newsController.updateNews,
+	)
+	.delete(
+		authController.protect,
+		authController.restrictTo('admin'),
+		newsController.deleteNews,
+	);
 
 module.exports = router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,33 +1,36 @@
-const express = require("express");
-const userController = require("./../controllers/userController");
-const authController = require("./../controllers/authController");
+const express = require('express');
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.post("/signup", authController.signup);
-router.post("/login", authController.login);
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.post("/forgotPassword", authController.forgotPassword);
-router.patch("/resetPassword/:token", authController.resetPassword);
-
+router.use(authController.protect);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
 router.patch(
-  "/updateMyPassword",
-  authController.protect,
-  authController.updatePassword,
+	'/updateMe',
+	userController.uploadUserPhoto,
+	userController.resizeUserPhoto,
+	userController.updateMe,
 );
-
-router.patch("/updateMe", authController.protect, userController.updateMe);
-router.delete("/deleteMe", authController.protect, userController.deleteMe);
-
-router
-  .route("/")
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.use(authController.restrictTo('admin'));
 
 router
-  .route("/:id")
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+	.route('/')
+	.get(userController.getAllUsers)
+	.post(userController.createUser);
+
+router
+	.route('/:id')
+	.get(userController.getUser)
+	.patch(userController.updateUser)
+	.delete(userController.deleteUser);
 
 module.exports = router;
